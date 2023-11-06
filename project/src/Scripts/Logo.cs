21f8aa0cd10539b993fa2590f3cd5a9f97d21e2e
Thomas-X <thomaszwarts@gmail.com>
2023-11-06 21:06:26 +0100

    add logo

--- project/src/Scripts/Logo.cs
+++ project/src/Scripts/Logo.cs
@@ -1,16 +1,16 @@
 namespace SCALE.Scripts;
 
-public partial class Logo : AnimatedSprite2D
+public partial class Logo : Sprite2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        Play();
-        PlayBackwards();
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		//Play();
+		//PlayBackwards();
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 }
