eb23d0b33b8159a1812e00795631a292011cf15a
Thomas-X <thomaszwarts@gmail.com>
2023-11-06 21:33:28 +0100

    add button sounds

--- project/src/Scripts/Buttons/ButtonPressedMove.cs
+++ project/src/Scripts/Buttons/ButtonPressedMove.cs
@@ -1,11 +1,37 @@
+using SCALE.Constants;
+
 namespace SCALE.Scripts.Buttons;
 
 public partial class ButtonPressedMove : Button
 {
+    private bool isHovered = false;
+    private bool playedSound = false;
+    private EventBus _eventBus = null!;
+
     public override void _EnterTree()
     {
         ButtonDown += OnButtonDown;
         ButtonUp += OnButtonUp;
+        _eventBus = this.GetEventBus();
+    }
+
+    public override void _Process(double delta)
+    {
+        if (isHovered != IsHovered())
+        {
+            isHovered = IsHovered();
+
+            if (isHovered && !playedSound)
+            {
+                _eventBus.EmitOnRequestToPlaySound(AudioPaths.UI_SOUNDPACK_WAV_MODERN7_WAV);
+                playedSound = true;
+            }
+        }
+
+        if (isHovered == false)
+        {
+            playedSound = false;
+        }
     }
 
     private void OnButtonUp()
@@ -16,5 +42,7 @@ public partial class ButtonPressedMove : Button
     private void OnButtonDown()
     {
         GlobalPosition += new Vector2(0, 5);
+        
+        _eventBus.EmitOnRequestToPlaySound(AudioPaths.UI_SOUNDPACK_WAV_MODERN8_WAV);
     }
 }
--- project/src/Scripts/Buttons/ExitToDesktopButton.cs
+++ project/src/Scripts/Buttons/ExitToDesktopButton.cs
@@ -2,18 +2,9 @@ namespace SCALE.Scripts.Buttons;
 
 public partial class ExitToDesktopButton : ButtonPressedMove
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
-
     public override void _Pressed()
     {
+        base._Pressed();
         var tree = GetTree();
         tree.Root.PropagateNotification((int)NotificationWMCloseRequest);
         tree.Quit();
