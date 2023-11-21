9fbfb4b4ba7091db17a5b6ef2ffe9ee71a4fe349
Thomas-X <thomaszwarts@gmail.com>
2023-11-21 16:37:09 +0100

    remove last source generator; use packaged up version instead

--- project/src/Constants/Scenes.cs
+++ project/src/Constants/Scenes.cs
@@ -1,10 +1,8 @@
-﻿namespace SCALE.Constants;
+﻿using ScenesPathSourceGenerator;
+
+namespace SCALE.Constants;
 
 [GenerateScenePaths]
 public partial class Scenes
 {
-    public static PackedScene GetSceneForFilePath(string filePath)
-    {
-        return ResourceLoader.Load<PackedScene>(filePath);
-    } 
 }
